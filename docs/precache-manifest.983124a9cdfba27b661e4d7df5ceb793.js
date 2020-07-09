4439c85e3e89564a77fa2b13e116ae570b0b0af3
Alex <ayllon12@gmail.com>
2020-07-09 11:41:03 +0200

    added docs

---
+++ docs/precache-manifest.983124a9cdfba27b661e4d7df5ceb793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081eb1c9141e9cc9edf1e249ef24723e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c02c3da54815098c3079",
+    "url": "/static/css/main.e13a6c2e.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c02c3da54815098c3079",
+    "url": "/static/js/main.765cf7ed.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
