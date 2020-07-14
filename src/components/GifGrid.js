1a3c5fa2d9db6d30a99978708d28e8756a877ad5
Alex <ayllon12@gmail.com>
2020-07-14 10:44:38 +0200

    Added tests to GifGrid component

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import { GifGridItem } from './GifGridItem';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
@@ -25,4 +26,9 @@ export const GifGrid = ({ category }) => {
             </div>
         </>
     )
+}
+
+
+GifGrid.propTypes = {
+    category: PropTypes.string.isRequired
 }
---
+++ src/tests/components/GifGrid.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { GifGrid } from '../../components/GifGrid';
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+jest.mock('../../hooks/useFetchGifs');
+
+
+describe('Pruebas en el <GifGrid />', () => {
+
+    const category = 'Goku';
+    test('should show correctly', () => {
+
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true
+        })
+
+        const wrapper = shallow(<GifGrid category={category} />);
+
+        expect( wrapper ).toMatchSnapshot();
+        
+    })
+
+    test('should show items when img loaded with useFetchGifs', () => {
+        const gifs = [{
+            id: 'ABC',
+            url: 'https:localhost',
+            title: 'random'
+        }]
+        useFetchGifs.mockReturnValue({
+            data: gifs,
+            loading: false
+        })
+
+        const wrapper = shallow(<GifGrid category={category} />);
+
+        expect( wrapper ).toMatchSnapshot();
+    })
+    
+    
+})
