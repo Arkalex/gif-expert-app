db569e21a4e0a0bd1ff5d36d74a575a933758995
Alex <ayllon12@gmail.com>
2020-07-14 11:10:10 +0200

    More tests on GifGrid component

--- src/tests/components/GifGrid.test.js
+++ src/tests/components/GifGrid.test.js
@@ -33,8 +33,9 @@ describe('Pruebas en el <GifGrid />', () => {
         })
 
         const wrapper = shallow(<GifGrid category={category} />);
-
         expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find('p').exists() ).toBe(false);
+        expect( wrapper.find('GifGridItem').length ).toBe(gifs.length);
     })
     
     
